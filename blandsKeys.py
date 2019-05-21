f5d4da209e0e9d58c462c7ad78b307ce8eff852e
hxstpro <37120801+hxstpro@users.noreply.github.com>
2019-05-21 12:16:18 -0400

    Update blandsKeys.py

--- blandsKeys.py
+++ blandsKeys.py
@@ -8,9 +8,7 @@ for i in range(0, 10):
         f.write(d.entries[i].description)
         i = i+1
 
-f = re.sub("<.*?>", "", text)
 f.close()
 
 print('Downloading golden keys...')
 print('Complete.')
-
